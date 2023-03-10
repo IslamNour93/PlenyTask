ef3cd93b30024a91f148be6b6c73ed3624e4253c
Islam NourEldin <99802390+IslamNour93@users.noreply.github.com>
2023-03-10 15:39:00 +0200

    initial commit

---
+++ PlenyTask/Model/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  PlenyTask
+//
+//  Created by Islam NourEldin on 10/03/2023.
+//
+
+import Foundation
+
+struct Post{
+    
+}
---
+++ PlenyTask/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  PlenyTask
+//
+//  Created by Islam NourEldin on 10/03/2023.
+//
+
+import Foundation
+
+struct User{
+    
+}
