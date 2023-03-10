966c6accd422efd94b91236d8463fd38ad9f88ce
Islam NourEldin <99802390+IslamNour93@users.noreply.github.com>
2023-03-10 14:59:45 +0200

    Initial Commit

---
+++ PlenyTask/PlenyTaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlenyTaskApp.swift
+//  PlenyTask
+//
+//  Created by Islam NourEldin on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlenyTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
